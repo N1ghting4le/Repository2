2bd9763ad9d31afe9a9d507eaa311d7d8b2b1736
N1ghting4le <nikansoloveib@gmail.com>
2022-10-28 19:02:29 +0300

    program about films v 1.0.1

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,37 @@
 "use strict";
 
-let numberOfFilms = +prompt("How many films you have watched", "");
+let answers = [];
+
+let questions = ["How many films you have watched?",
+                "Which film did you watch last time?",
+                "What is your mark for this  film?"];
+
+for (let i = 0; i < questions.length; i++) {
+    answers[i] = prompt(questions[i]);
+    if (answers === null || answers[i] === null || answers[i].length > 50 || answers[i] === "") {
+        i--;
+    }
+}
 
 let personalMovieDB = {
-    count: numberOfFilms,
+    count: answers[0],
     movies: {},
     actors: {},
     genres: [],
     privat: false
 };
 
-let a = prompt("Which film did you watch last time?", ""),
-    b = prompt("What is your mark for this  film?", "");
+if (personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    alert("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    alert("Вы киноман");
+} else {
+    alert("Произошла ошибка");
+    personalMovieDB.count = null; 
+}
 
-personalMovieDB.movies[a] = b;
+personalMovieDB.movies[answers[1]] = answers[2];
 
 console.log(personalMovieDB);
